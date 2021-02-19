3b3c94c94a52eb754e55aed019aedeccec4b0333
swloewen <79286600+swloewen@users.noreply.github.com>
2021-02-19 12:26:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,12 @@
 		    	echo "<p>".$key."</p>";
 		    }
 	    }
+      function tax_calc($amount,$tax){
+    	$calculate_tax = $amount*$tax;
+    	$amount = round($amount+$calculate_tax,2);
+    	return $amount;
+    }
+    echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
